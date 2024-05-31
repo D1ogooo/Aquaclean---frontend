38a0f062fc8f3cb42c4a6509b8e049bfde2b84b0
D1ogooo <diiogocosta11@gmail.com>
2024-05-30 23:31:49 -0300

    Nosso trabalho em andamento...

--- src/components/NossoTrabalho/index.jsx
+++ src/components/NossoTrabalho/index.jsx
@@ -5,40 +5,28 @@ function NossoTrabalho () {
  return (
   <>
    <div className="pai">
-    <div className="container">
     <h1 className='title'>AquaClean</h1>
-
-     <div className="left">
+    <div className="container">
+     <div className="left"> {/* início do left content */}
+      <div className='internal_left_one'>
       <section>
        <div className='circle'>
         <p>01</p>
        </div>
-       <p className='texto'>Um drone submarino capaz de pegar o lixar e armazenar 
+       <p className='texto'>Um drone submarino capaz de pegar o lixo e armazenar 
         para depois ser despejado de uma maneira correta</p>
       </section>
       <section>
        <div className='circle'>
-        <p>02</p>
+        <p>01</p>
        </div>
-       <p className='texto'>Vai ser movido a bateria sustentável,
-        vai ser construido a base resido recicláveis.</p>
+       <p className='texto'>Um drone submarino capaz de pegar o lixo e armazenar 
+        para depois ser despejado de uma maneira correta</p>
       </section>
-      <section>
-      <div className='circle'>
-        <p>03</p>
-       </div>
-       <p className='texto'>sistema inteligente e sistema de filtragem para identificar o lixo e filtra-lo</p>
-      </section>
-      <section>
-      <div className='circle'>
-        <p>04</p>
-       </div>
-       <p className='texto'>Sensores avançado para detectar o lixo e navegação autónoma para 
-       ter o mínimo controle humano sobre o drone</p>
-      </section>
-     </div>
+      </div>
+     </div>  {/* fim do left content */}
      <div className="right">
-      <img src={RobotImage} id='image_right'/>
+      {/* <img src={RobotImage} id='image_right'/> */}
      </div>
     </div>
    </div>
